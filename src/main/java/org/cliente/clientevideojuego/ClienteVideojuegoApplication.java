c84f6cd1768e0d95c46f2b79371444cef73f1b6a
Miguel <92276707+miike96@users.noreply.github.com>
2024-02-20 14:08:07 +0100

    Prueba

---
+++ src/main/java/org/cliente/clientevideojuego/ClienteVideojuegoApplication.java
@@ -0,0 +1,13 @@
+package org.cliente.clientevideojuego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteVideojuegoApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
