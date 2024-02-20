55362d60f0acce5281ded9d60c1678b7dfa3101a
Miguel <92276707+miike96@users.noreply.github.com>
2024-02-20 14:08:07 +0100

    Prueba

---
+++ src/test/java/org/cliente/clientevideojuego/ClienteVideojuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cliente.clientevideojuego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteVideojuegoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
